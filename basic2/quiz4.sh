e75574727609e054c0e80c6f0f709f500736fc76
user <@gmail.com>
2024-04-03 12:37:32 +0900

    gogogogogog

---
+++ basic2/quiz4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir myDir
+
+echo "..." > myFile.txt
+mv myFile.txt myDir
+
+rm myFile.txt
+
+rmdir myDir
---
+++ basic2/quiz5.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Line 1" > notes.txt
+echo "Line 2" >> notes.txt
+echo "Line 3" >> notes.txt
+
+cat notes.txt
+
+rm notes.txt
