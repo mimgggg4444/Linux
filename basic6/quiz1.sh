b45d645ae0863f4c144b01341778f1a7fd371f7e
MIN <66135779+mimgggg4444@users.noreply.github.com>
2024-05-08 11:39:20 +0900

    Update quiz1.sh

--- basic6/quiz1.sh
+++ basic6/quiz1.sh
@@ -3,7 +3,11 @@
 # 파일명: quiz1.sh
 
 # 새 사용자 testuser1 추가
-useradd testuser2
+# useradd testuser2
 
 # /etc/passwd에서 testuser1 정보 출력
-grep "testuser2" /etc/passwd
+# grep "testuser2" /etc/passwd
+
+
+adduser --home /home/testuser1 testuser1
+tail -1 /etc/passwd
