ca85fca58aac8246f97f5583a1dc07e368cad2e9
MIN <66135779+mimgggg4444@users.noreply.github.com>
2024-05-08 11:41:07 +0900

    Update quiz5.sh

--- basic6/quiz5.sh
+++ basic6/quiz5.sh
@@ -3,13 +3,14 @@
 # 파일명: quiz5.sh
 
 # 그룹 mygroup 존재 확인, 없으면 생성
-getent group mygroup &>/dev/null || groupadd mygroup
+# getent group mygroup &>/dev/null || groupadd mygroup
 
 # 사용자 testuser4 추가 (기존에 없으면 추가)
-id -u testuser4 &>/dev/null || useradd testuser4
+#id -u testuser4 &>/dev/null || useradd testuser4
 
 # testuser4를 mygroup에 추가
-usermod -a -G mygroup testuser4
+#usermod -a -G mygroup testuser4
 
+gpasswd -a testuser4 mygroup
 # mygroup 정보 확인
 grep "mygroup" /etc/group
