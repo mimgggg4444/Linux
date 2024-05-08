0cf017932a9fa190d3eb9c72712227411034bcca
MIN <66135779+mimgggg4444@users.noreply.github.com>
2024-05-08 11:39:41 +0900

    Update quiz2.sh

--- basic6/quiz2.sh
+++ basic6/quiz2.sh
@@ -6,14 +6,18 @@
 
 
 # idk
-id -u testuser2 &>/dev/null || useradd -m -d /home/newhome -s /bin/bash testuser2
+#id -u testuser2 &>/dev/null || useradd -m -d /home/newhome -s /bin/bash testuser2
 
-usermod -d /tmp testuser2
+#usermod -d /tmp testuser2
 
-usermod -d /home/newhome -m testuser2
+#usermod -d /home/newhome -m testuser2
 
 
 # 홈 디렉터리 변경
 
 # 변경된 정보 확인
-grep "testuser2" /etc/passwd
+#grep "testuser2" /etc/passwd
+
+
+usermod --home /home/newhome testuser2
+tail -1 /etc/passwd
