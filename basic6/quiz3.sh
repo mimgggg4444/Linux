851f41b12f0b35830d9395fb488e322b92bbbe04
MIN <66135779+mimgggg4444@users.noreply.github.com>
2024-05-08 11:40:10 +0900

    Update quiz3.sh

--- basic6/quiz3.sh
+++ basic6/quiz3.sh
@@ -3,7 +3,11 @@
 # 파일명: quiz3.sh
 
 # 사용자 testuser2 삭제
-userdel testuser2
+#userdel testuser2
 
 # /etc/passwd 파일에서 testuser2 정보 확인
+#grep "testuser2" /etc/passwd || echo "User testuser2 not found"
+
+
+userdel testuser2
 grep "testuser2" /etc/passwd || echo "User testuser2 not found"
